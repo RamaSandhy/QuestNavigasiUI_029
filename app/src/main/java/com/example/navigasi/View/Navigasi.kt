90ffe65de24d6550a36f8e661db2c3308840bb1b
RamaSandhy <rshandy07@gmail.com>
2025-11-08 13:04:40 +0700

    menambahkan import

--- app/src/main/java/com/example/navigasi/View/Navigasi.kt
+++ app/src/main/java/com/example/navigasi/View/Navigasi.kt
@@ -1,12 +1,16 @@
 package com.example.navigasi.View
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import java.lang.reflect.Modifier
+import com.example.navigation.View.FormIsian
+import com.example.navigation.View.TampilData
+
 
 enum class Navigasi {
     Formulirku,
