53ea1477c9bb3823b624c582ac9189f89f547f48
RamaSandhy <rshandy07@gmail.com>
2025-11-08 13:20:05 +0700

    tambahkan Column dengan padding isiRuang dan arrangement SpaceBetween

--- app/src/main/java/com/example/navigasi/View/TampilData.kt
+++ app/src/main/java/com/example/navigasi/View/TampilData.kt
@@ -43,6 +43,10 @@ fun TampilData(
                 colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.teal_700))
             )
         }
+    ){isiRuang ->
+        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
 
 }
 
